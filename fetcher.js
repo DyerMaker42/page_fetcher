50dc43992b56e93c1449dad89068e4a3b602663b
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-09-24 04:05:51 +0000

    updated

--- fetcher.js
+++ fetcher.js
@@ -10,26 +10,14 @@ const fs = require('fs');
 
 let input = process.argv;
 
-//console.log(input[2], "URL", input[3], "destination")
-
 request(input[2], (error, response, body) => {
   if (!error) {
-    //console.log(response)
-    
-    fs.writeFile(input[3],body, (err) => {
-      //console.log(response)
-      if (err) {throw err};
-      // if(input[3]){
-      //   throw err
-      // }
-      //console.log("the file has been updated")
+    fs.writeFile(input[3], body, (err) => {
+      if (err) { throw err };
     });
-    let bytes;// returns an object called stats, with key of size
-    //return console.log(`Downloaded and saved ${bytes.size} bytes to ${input[3]}`);
   }
-
-})
-const getFileSize = function(file) {
+});
+const getFileSize = function (file) {
   let stats = fs.statSync(input[3])
   let fileSizeinBytes = stats["size"]
   return fileSizeinBytes
